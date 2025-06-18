35b02cc33e6c1cf418b92c16d99ffa30c45b9bdc
farhanshah24 <farhanshahalm84@gmail.com>
2025-06-18 17:26:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    const preloader = document.getElementById("preloader");
+    preloader.style.opacity = "0";
+    setTimeout(function () {
+      preloader.style.display = "none";
+    }, 500); // matches the CSS transition
+  }, 7000); // No artificial delay
+});
+
+function toggleNav() {
+  document.querySelector(".nav-links").classList.toggle("show");
+}
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", function () {
+    document.querySelector(".nav-links").classList.remove("show");
+  });
+});
+
+const navbar = document.querySelector(".navbar"); // Adjust selector as needed
+document.documentElement.style.setProperty(
+  "--navbar-height",
+  navbar.offsetHeight + "px"
+);
